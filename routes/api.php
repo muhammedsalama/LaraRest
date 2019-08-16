decb5670f52cc81e50e6ea340237f52893d43556
Mohamed Salama Shakir <salamam301@yahoo.com>
2019-08-16 17:36:38 +0200

    First endpoint and manual testing

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
+use Dingo\Api\Routing\Router;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+$router = app(Router::class);
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+$router->version('v1',function (Router $router){
+   $router->group(['namespace'=>'App\Http\Controllers'],function (Router $router){
+        $router->get('test','ServerController@test');
+   });
 });
